9a1b31db382824b590ed82e624689d7bbc9c0205
ailadson <aladsonjr@gmail.com>
2015-07-28 16:06:56 -0500

    Integrate front and back end

--- app/handlers/helpers/jinja.py
+++
@@ -1,5 +0,0 @@
-import jinja2
-import os
-
-jinja = jinja2.Environment(autoescape=True,
-                           loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__))))
--- app/handlers/homePageHandler.py
+++ app/handlers/homePageHandler.py
@@ -1,9 +1,15 @@
-import sys
-sys.path.insert(0, './handlers/helpers')
+import os
+import jinja2
 
 from baseHandler import BaseHandler
-from jinja import jinja
+
+
+jinja = jinja2.Environment(autoescape=True,
+                        loader=jinja2.FileSystemLoader("./html"))
+    
+
 
 class HomePageHandler(BaseHandler):
     def get(self):
-        self.response.write("Hello Hive Exchange<br>")
+        home = jinja.get_template('index.html')
+        self.response.write(home.render())
