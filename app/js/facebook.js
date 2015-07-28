1fc0aa107c06f8a2c0de475f249aaae461f7d6c5
ailadson <aladsonjr@gmail.com>
2015-07-28 17:11:31 -0500

    Add FB login button

---
+++ app/js/facebook.js
@@ -0,0 +1,42 @@
+function FBstatusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      alert("Not Authorized")
+    } else {
+      alert("Not Logged into FB") 
+    }
+  }
+    
+  function FBcheckLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+      
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '511140802368207',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+    
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      alert('you are' + response.name + '!');
+    });
+  }
